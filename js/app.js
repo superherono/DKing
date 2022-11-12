2eb62e766051b1639d1aee485a20b59aa8021cd6
superherono <dmitrii.morozan@gmail.com>
2022-11-12 11:16:15 +0200

    axllax

--- js/app.js
+++ js/app.js
@@ -40,6 +40,17 @@
     function getHash() {
         if (location.hash) return location.hash.replace("#", "");
     }
+    function fullVHfix() {
+        const fullScreens = document.querySelectorAll("[data-fullscreen]");
+        if (fullScreens.length && isMobile.any()) {
+            window.addEventListener("resize", fixHeight);
+            function fixHeight() {
+                let vh = .01 * window.innerHeight;
+                document.documentElement.style.setProperty("--vh", `${vh}px`);
+            }
+            fixHeight();
+        }
+    }
     let _slideUp = (target, duration = 500, showmore = 0) => {
         if (!target.classList.contains("_slide")) {
             target.classList.add("_slide");
@@ -4526,6 +4537,7 @@
     addTouchClass();
     menuInit();
     menuSublistsInit();
+    fullVHfix();
     formFieldsInit({
         viewPass: false,
         autoHeight: false
